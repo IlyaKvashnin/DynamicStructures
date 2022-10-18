f7076cc9d4660a13d8c762e269c56f65a97c070f
ilyakvashnin <27romashka05@gmail.com>
2022-10-18 22:04:36 +0500

    ended stack and merged queue

--- DynamicStructure/DynamicStructure/DynamicStructure.Core/SinglyLinkedList/ListNode.cs
+++ DynamicStructure/DynamicStructure/DynamicStructure.Core/SinglyLinkedList/ListNode.cs
@@ -11,16 +11,16 @@ namespace DynamicStructure.DynamicStructure.Core.SinglyLinkedList
         private ListNode<T> next;
         private T item;
 
-        public ListNode<T> Next
+        public ListNode<T>? Next
         {
-            get { return next; }
-            set { next = value; }
+            get {return next;}
+            set { next = value;}
         }
 
         public T Item
         {
-            get { return item; }
-            set { item = value; }
+            get {return item;}
+            set { item = value;}
         }
 
         public ListNode(T item)
@@ -39,6 +39,30 @@ namespace DynamicStructure.DynamicStructure.Core.SinglyLinkedList
             if (item == null)
                 return string.Empty;
             return item.ToString();
+        }
+    
+        public T? First()
+        {
+            return Item;
+        }
+        public T? Last()
+        {
+            ListNode<T> currentItem = this;
+            while (currentItem.Next != null)
+            {
+                currentItem = currentItem.Next;
+            }
+            return currentItem.Item;
+        }
+        public T? ElementAt(uint index)
+        {
+            ListNode<T> currentItem = this;
+            for (int i = 0; i < index;i++)
+            {
+                if (currentItem.Next != null)
+                    currentItem = currentItem.Next;
+            }
+            return currentItem.Item;
         }
     }
 }
--- DynamicStructure/DynamicStructure/Program.cs
+++ DynamicStructure/DynamicStructure/Program.cs
@@ -1,12 +1,7 @@
 ﻿using DynamicStructure.DynamicStructure.ConsoleUI;
-using System;
-using DynamicStructure.DynamicStructure.Core.Stack;
-using DynamicStructure.DynamicStructure.FileWorker;
+using DynamicStructure.DynamicStructure.Core.Queue;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Numerics;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace DynamicStructure
 {
@@ -17,11 +12,6 @@ namespace DynamicStructure
 
             MenuLogic mainMenu = new MenuLogic(MainMenu.mainMenu);
             mainMenu.Run();
-            //while (true) //Бесконечный цикл
-            //{
-            //    Console.Write("Введите выражение: "); //Предлагаем ввести выражение
-            //    Console.WriteLine(PostfixNotation.Calculate(Console.ReadLine())); //Считываем, и выводим результат
-            //}
         }
 
 
