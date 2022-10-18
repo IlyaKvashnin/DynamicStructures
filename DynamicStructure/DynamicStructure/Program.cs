0498987281544238497b394272c32a91d8ccd909
ilyakvashnin <27romashka05@gmail.com>
2022-10-18 12:11:40 +0500

    123

--- DynamicStructure/DynamicStructure/Program.cs
+++ DynamicStructure/DynamicStructure/Program.cs
@@ -1,11 +1,6 @@
 ﻿using DynamicStructure.DynamicStructure.ConsoleUI;
-using DynamicStructure.DynamicStructure.Core.Stack;
-using DynamicStructure.DynamicStructure.FileWorker;
+using DynamicStructure.DynamicStructure.Core.Queue;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Numerics;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace DynamicStructure
 {
@@ -16,6 +11,11 @@ namespace DynamicStructure
 
             MenuLogic mainMenu = new MenuLogic(MainMenu.mainMenu);
             mainMenu.Run();
+            //while (true) //Бесконечный цикл
+            //{
+            //    Console.Write("Введите выражение: "); //Предлагаем ввести выражение
+            //    Console.WriteLine(PostfixNotation.Calculate(Console.ReadLine())); //Считываем, и выводим результат
+            //}
         }
 
 
